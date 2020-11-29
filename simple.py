8db4e27e1c42006512136c75fe9f6bc5eeab2a0c
Gaetan Manchon <gmanchon@gmail.com>
2020-11-29 20:53:35 +0100

    u

--- simple.py
+++ simple.py
@@ -8,6 +8,11 @@ def index():
     return {"key": "value"}
 
 
+@app.get("/predict")
+def create_fare(a, b, c):
+    return {"key": a + b + c}
+
+
 @app.get("/hello/{name}")
 def hello(name):
     return {"key": f"Hello, {name}!"}
